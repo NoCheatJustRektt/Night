f1a63fdf84cbd641574b3fe64a64226808ab2b07
Luis <153270311+LuisJuniorA@users.noreply.github.com>
2025-12-05 06:25:06 +0100

    oui

--- src/view/snake-widget.php
+++ src/view/snake-widget.php
@@ -1,6 +1,6 @@
 <div id="snake-container">
     <h2 id="snake-title">Snake Game</h2>
-    <p id="snake-instructions">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suivez les flèches pour guider votre serpent et mangez les pommes rouges !</p>
+    <p id="snake-instructions">Apple nous attaque ! Après les rumeurs d’obsolescence programmée, voilà qu’ils s’en prennent directement à nos appareils ! Prépare-toi à combattre les ralentissements, les batteries fatiguées et les applis gourmandes pour survivre !</p>
     <canvas id="snake-canvas" width="300" height="300"></canvas>
     <div id="snake-score">Score : 0</div>
     <div id="snake-controls">
